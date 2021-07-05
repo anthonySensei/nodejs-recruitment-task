d338122ad214669646282b3f993b2c74ba5aaa8c
Anton Shevchuk <w.s.f.antonshevchuk@gmail.com>
2021-07-05 13:49:07 +0300

    added swagger for api documentation, configured cors to recieve request from swagger and another origins

---
+++ movie-service/src/config/cors.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express';
+
+export default (req: Request, res: Response, next: NextFunction) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, PATCH, PUT, POST, DELETE, OPTIONS'
+  );
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  next();
+};
